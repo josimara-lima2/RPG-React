8dd87ab550d268130ce788d01dcfc6456c27b93a
Josimara <josybehlul@gmail.com>
2021-12-01 09:42:25 -0300

    adicionando movimentacao ao Character

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import * as C from './App.styles'
-
+import { Character } from './components/Character/index';
+import { useCharacter } from './hooks/useCharacter';
+import { useEffect } from 'react';
 const App = () => {
+  const char = useCharacter()
+
+  useEffect(()=>{
+    window.addEventListener('keydown',handleKeydown)
+  },[])
+
+  const handleKeydown = (e:KeyboardEvent) => {
+   
+    switch(e.code){
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft()
+        break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight()
+        break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp()
+        break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown()
+        break;
+      default:
+        break;
+    }
+  }
+   
   return (
     <C.Container>
       <C.Map>
-        ...
+        <Character x={char.x} y={char.y}/>
       </C.Map>
     </C.Container>
   );
